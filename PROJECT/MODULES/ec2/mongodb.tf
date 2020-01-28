f9b943eb856d79fae69744973177e0fdb565680b
KalluruS <>
2020-01-28 08:44:37 +0530

    Adding files

--- PROJECT/MODULES/ec2/mongodb.tf
+++ PROJECT/MODULES/ec2/mongodb.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "mongodb" {
   ami                       = data.aws_ami.ami-public.id
   instance_type             = var.INSTANCE_TYPE["mongodb"]
-  vpc_security_group_ids    = [var.SG-SSH,var.SG-MONGO]
+  subnet_id                 = var.SUBNETS[0]
+  vpc_security_group_ids    = [var.SG-SSH, var.SG-MONGO]
 }
