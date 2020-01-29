4e311da80ee815435462af7e8371fb244e076f93
KalluruS <>
2020-01-29 07:43:58 +0530

    Adding files

--- PROJECT/MODULES/ec2/mongodb.tf
+++ PROJECT/MODULES/ec2/mongodb.tf
@@ -3,4 +3,23 @@ resource "aws_instance" "mongodb" {
   instance_type             = var.INSTANCE_TYPE["mongodb"]
   subnet_id                 = var.SUBNETS[0]
   vpc_security_group_ids    = [var.SG-SSH, var.SG-MONGO]
+  key_name                  = "devops"
+
+  tags                      = {
+    Name                    = "mongo-db-instance"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host = self.public_ip
+      user = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "yum install ansible -y",
+      "echo localhost >/tmp/hosts",
+      "ansible-pull -i /tmp/hosts -U https://rkb03:password@gitlab.com/d44/ansible.git playbooks/project.yml -t mongodb"
+    ]
+  }
 }
